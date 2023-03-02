f526db5b6fbe2acb82a292f2b9d3c86481b1de7b
mohammad sobuj <mohammadsobuj1m>
2023-03-03 03:16:30 +0600

    details section done

--- js/alunivers.js
+++ js/alunivers.js
@@ -1,15 +1,18 @@
-const loadData = ()=>{
+const loadData = (limit)=>{
     fetch('https://openapi.programming-hero.com/api/ai/tools')
     .then(res => res.json())
-    .then(data =>displayData(data.data.tools))
+    .then(data =>displayData(data.data.tools, limit))
 }
 
-const displayData =(data)=>{
+// cardSection start
+
+const displayData =(data, limit)=>{
     
 const cardSection = document.getElementById('card-section')
+cardSection.innerHTML='';
 const seeallBtn =document.getElementById('seeall-btn')
 
-if( data.length > 6){
+if(limit && data.length > 6){
     data= data.slice(0, 6)
     seeallBtn.classList.remove('hidden')
     }
@@ -43,7 +46,7 @@ div.innerHTML= `
         <p class="mt-2"><i class="fa-regular fa-calendar-days"></i> ${published_in}</p>
         </div>
         <div class="card-actions  mt-4 ">  
-        <button class=" text-orange-500 " onclick=" loadDetailes('${id}')"><i class="fa-solid fa-arrow-right"></i></button>
+        <label for="my-modal-3" class=" text-orange-500 " onclick=" loadDetailes('${id}')"><i class="fa-solid fa-arrow-right"></i></label>
       </div>
 </div>
 </div>
@@ -55,11 +58,28 @@ div.innerHTML= `
     });
 }
 
+// cardSection end 
+
+
+// see all btn section  
+
+const prosearchData =(limit)=>{
+
+loadData(limit)    
+}
+
 
 document.getElementById('showall-btn').addEventListener('click', function(){
-    console.log('click')
+    prosearchData()
 })
 
+loadData('06')
+
+// see all btn section end 
+
+
+
+// Detailes section start
 
 const loadDetailes = (id )=>{
     
@@ -71,14 +91,65 @@ const loadDetailes = (id )=>{
 }
 
 const displayDetials =(data)=>{
+const {description, pricing,  features, integrations, image_link, input_output_examples
+}= data;
 
-    
-    console.log(data)
+console.log(data)
+
+const DetailesData = document.getElementById('details')
+DetailesData.innerHTML=`
+<p class="font-semibold text-xl ">Description : ${description}  </p>
+<div class="flex mt-5 justify-between  gap-5">
+<p class="font-bold text-green-400 border-0 p-2 shadow-2xl"> ${pricing[0].price}  ${pricing[0].plan  }</P>
+<p class="font-bold text-red-400  border-0 p-2 shadow-2xl"> ${pricing[1].price}  ${pricing[1].plan  }</P>
+<p class="font-bold text-blue-300 border-0  p-2 shadow-2xl" > ${pricing[2].price}  ${pricing[2].plan  }</P>
+
+</div>
+
+<div class="flex justify-between">
+<div>
+<p class="font-bold text-xl mt-5">Features</p>
+<ol class="font-mono ...">
+<li>${ features[1].feature_name} </li>
+<li>${ features[2].feature_name} </li>
+<li>${ features[3].feature_name} </li>
+
+</ol>
+</div>
+
+<div>
+<p class="font-bold text-xl mt-5">Integrations</p>
+<ul class="font-mono ...">
+<li>${ integrations[0]} </li>
+<li>${ integrations[1]} </li>
+<li>${integrations[2]} </li>
+
+</ul>
+
+</div>
+
+</div>
+
+<div>
+<img src="${image_link[0]}" alt="">
+<h2 class="font-bold text-xl mt-5 text-center">${input_output_examples[0].input}</h2>
+<h2 class=" font-mono ... text-xl mt-3 w-[75%] mx-auto">${input_output_examples[0].output}</h2>
+
+</div>
+
+
+
+
+
+`
+
+   
 }
 
 
+// Detailes section end
 
 
 
 
-loadData()
+
