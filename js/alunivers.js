6b7941568bbeb6cea3ad1ac9e471198359a95a7f
mohammad sobuj <mohammadsobuj1m>
2023-03-04 19:29:33 +0600

    my final commit done

--- js/alunivers.js
+++ js/alunivers.js
@@ -27,8 +27,8 @@ const cardSection = document.getElementById('card-section')
 cardSection.innerHTML='';
 const seeallBtn =document.getElementById('seeall-btn')
 
-if(limit && data.length > 2){
-    data= data.slice(0, 2)
+if(limit && data.length > 6){
+    data= data.slice(0, 6)
     seeallBtn.classList.remove('hidden')
     }
 else{
@@ -51,7 +51,7 @@ else{
     
 
       const div = document.createElement('div')
-      div.classList.add("border-2")
+      div.classList.add("border-4")
    
       
 div.innerHTML= `
@@ -62,13 +62,13 @@ div.innerHTML= `
           <img src="${image}" alt="Shoes" class="rounded-xl" />
         </figure>
         <div class="card-body ">
-          <h2 class="card-title ">Features</h2>
-          <p class="text-sm"> ${featurediv} </p>
+          <h2 class="card-title text-rose-500">Features</h2>
+          <p class="text-sm "> ${featurediv} </p>
           
           
         <div class="flex justify-between">
         <div>
-        <h2 class="text-xl font-bold mt-3">${name}</h2>
+        <h2 class="text-xl font-bold mt-3 text-rose-500">${name}</h2>
         <p class="mt-2"><i class="fa-regular fa-calendar-days"></i> ${published_in}</p>
         </div>
         <div class="card-actions  mt-4 ">  
@@ -150,7 +150,7 @@ const loadDetailes = (id )=>{
 
 }
 
-
+// single Detailes section 
 
 const displayDetials =(data )=>{
 
@@ -158,6 +158,7 @@ const {description, pricing,  features, integrations, image_link, input_output_e
   }= data;
 
 
+// integration loop 
 
 let integration =""
 
@@ -170,7 +171,7 @@ if(integrations != null){
 }
 
 
-
+// modal section 
 
     const DetailesData = document.getElementById('details')
 DetailesData.innerHTML='';
@@ -180,25 +181,22 @@ DetailesData.innerHTML=`
 <div class="flex justify-between gap-5">
 
 
-<div class="border-2 border-orange-300  w-[60%] p-2 ">
+<div class="border-2 border-orange-300  bg-orange-50 rounded-lg w-[60%] p-2 ">
 
 <p class="font-semibold  ">Description : ${description}  </p>
-<div class="flex mt-5 justify-between  p-0  gap-2 text-sm">
+<div class="flex mt-5 justify-between  p-0  gap-2 text-sm bg-gray-100  ">
 
-<p class="font-bold text-red-400  border-0  shadow-2xl"> ${pricing ?pricing?.[0].price !="No cost" ? pricing?.[0].price :'free of cost': 'Not Found' }   ${pricing?pricing?.[0].plan : 'Not Found'} </P>
-<p class="font-bold text-red-400  border-0  shadow-2xl"> ${pricing ?pricing?.[1].price !="No cost" ? pricing?.[1].price :'free of cost': 'Not Found' }   ${pricing?pricing?.[1].plan : 'Not Found'} </P>
+<p class="font-bold text-red-400  border-0  shadow-2xl mt-4"> ${pricing ?pricing?.[0].price !="No cost" ? pricing?.[0].price :'free of cost': 'Not Found' }   ${pricing?pricing?.[0].plan : 'Not Found'} </P>
+<p class="font-bold text-red-400  border-0  shadow-2xl mt-4"> ${pricing ?pricing?.[1].price !="No cost" ? pricing?.[1].price :'free of cost': 'Not Found' }   ${pricing?pricing?.[1].plan : 'Not Found'} </P>
 
-
-
-
-<p class="font-bold text-blue-400  border-0  shadow-2xl"> ${pricing ?pricing[2].price : 'Not Found' }   ${pricing ?pricing[2].plan : 'Not Found'} </P>
+<p class="font-bold text-blue-400  border-0  shadow-2xl  "> ${pricing ?pricing[2].price : 'Not Found' }   ${pricing ?pricing[2].plan : 'Not Found'} </P>
 
 
 </div>
 
 <div class="flex justify-between gap-3">
 <div>
-<p class="font-bold text-xl mt-5">Features</p>
+<p class="font-bold text-xl mt-5  text-orange-400">Features</p>
 <ol class="font-mono ... text-sm mt-2">
 <li>  ${ features[1].feature_name} </li>
 <li>  ${ features[2].feature_name} </li>
@@ -208,9 +206,9 @@ DetailesData.innerHTML=`
 </div>
 
 <div>
-<p class="font-bold text-xl mt-5">Integrations</p>
+<p class="font-bold text-xl mt-5  text-orange-400">Integrations</p>
 <ul class="font-mono ... text-sm ml-4 mt-2">
-<p>  ${  integration ? integration : 'Not found'} </p>
+<p >  ${  integration ? integration : 'Not found'} </p>
 </ul>
 
 </div>
@@ -234,7 +232,7 @@ ${accuracy.score*100}%</h2>` : ''}
 
       <img src="${image_link[0]}" alt="">
       
-      <h2 class="font-bold text-xl mt-5 text-centers">${input_output_examples ? input_output_examples[0].input : 'Not Found' }</h2>
+      <h2 class="font-bold text-xl mt-5 text-centers  text-orange-400">${input_output_examples ? input_output_examples[0].input : 'Not Found' }</h2>
       <h2 class="  mt-5 text-centers text-sm">${input_output_examples ? input_output_examples[0].output : 'Not Found' }</h2>
 
 
@@ -253,15 +251,6 @@ ${accuracy.score*100}%</h2>` : ''}
 
 
 }
-
-
-
-
-
-
-
-
-
 
 
 
